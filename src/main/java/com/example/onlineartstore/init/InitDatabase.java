5198f24e97f8305313d6882c368d0ee3c5724aa3
Alina Muntian <Muntianalina2013@gmail.com>
2023-08-22 18:18:07 +0300

    Displaying uploaded pictures in the app

--- src/main/java/com/example/onlineartstore/init/InitDatabase.java
+++ src/main/java/com/example/onlineartstore/init/InitDatabase.java
@@ -6,6 +6,7 @@ import com.example.onlineartstore.service.RolesAndUserService;
 import com.example.onlineartstore.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
